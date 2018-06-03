390791d0203f927bb44cf8e17a06a579c4c79d1a
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2018-06-03 13:53:22 +0200

    Implemented trace of matrix

--- ModuleA.jl
+++ ModuleA.jl
@@ -3,5 +3,16 @@ module ModuleA
 export f
 
 f(X) = eigvals(X'*X) |> maximum |> sqrt 
+function g(X::AbstractMatrix)
+ tr = zero(eltype(X))
+ if size(X,1) == size(X,2)
+  for i in 1:size(X,1)
+   tr += X[i,i]
+  end
+  return tr
+ else
+  error("Not a square matrix.")
+ end
+end
 
 end
--- main.jl
+++ main.jl
@@ -1,10 +1,11 @@
 push!(LOAD_PATH, ".")
 
-import ModuleA: f
+import ModuleA: f,g
 
 function main()
 	X = rand(16,16)
 	f(X) |> println
+	g(X) |> println
 end
 
 main()
