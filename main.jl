1ec852807b91cb63464143085dd9c7d356e85e6e
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2018-06-03 14:39:07 +0200

    print frob

--- main.jl
+++ main.jl
@@ -1,11 +1,12 @@
 push!(LOAD_PATH, ".")
 
-import ModuleA: specnorm,tr
+import ModuleA: specnorm,tr,frob
 
 function main()
 	X = rand(16,16)
 	specnorm(X) |> println
 	tr(X) |> println
+	frob(X) |> println
 end
 
 main()
